f31b0d0a1180e4999c53771ba8f7f97bbfc82a7a
bpacana <40578722+bpacana@users.noreply.github.com>
2019-02-25 23:38:06 -0800

    Add files via upload

---
+++ client/build/precache-manifest.eac69ebe687c46b65c173a8e85b0d95a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca5b2a52494b46711fb8",
+    "url": "/static/js/main.ca5b2a52.chunk.js"
+  },
+  {
+    "revision": "22ace95661df10796353",
+    "url": "/static/js/1.22ace956.chunk.js"
+  },
+  {
+    "revision": "ca5b2a52494b46711fb8",
+    "url": "/static/css/main.db381132.chunk.css"
+  },
+  {
+    "revision": "314783672a3f62d4e361ff718a8c1061",
+    "url": "/index.html"
+  }
+];
